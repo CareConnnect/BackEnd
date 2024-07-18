02d117adc800dc47233a4cda474a857d7601088f
Juwon <juwon5196@gmail.com>
2024-07-18 12:17:56 +0900

    [주원] Protector 프로젝트 생성
    보호자용

---
+++ Protector/src/main/java/com/careconnect/protector/ProtectorApplication.java
@@ -0,0 +1,13 @@
+package com.careconnect.protector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectorApplication.class, args);
+	}
+
+}
